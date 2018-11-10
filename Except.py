d52fa1100355ec65f78267ffcf8cb747853f350b
Daniel Venturini <danielventurini021@gmail.com>
2018-11-09 23:29:52 -0200

    Adicionando classe de Exceções

---
+++ Except.py
@@ -0,0 +1,34 @@
+# file with Exceptions
+
+class MyGenericErr(Exception):
+
+	def __init__(self, code):
+		self.code = code
+
+	@property
+	def code(self):
+		return self._code
+
+	@code.setter
+	def code(self, code):
+		self._code = code
+
+class TestErr(MyGenericErr):
+
+	def __str__(self):
+		return 'npm test err with code ' + str(self.code)
+
+class InstallErr(MyGenericErr):
+
+	def __str__(self):
+		return 'npm install err with code ' + str(self.code)
+
+class ScriptTestErr(MyGenericErr):
+
+	# code 1 if package.json hasn't scripts->test
+	# code 0 if package.json doesn't specified test: 'echo \"Error: no test specified\" && exit 1'
+	def __str__(self):
+		if self._code:
+			return 'script test hasn\'t key test'
+		else:
+			return 'script test hasn\'t specify test'
