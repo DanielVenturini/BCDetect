05dcf59b60e4c1c0278730ecd993bd4663bfd968
Daniel Venturini <danielventurini021@gmail.com>
2018-11-22 02:18:43 -0200

    Lógica alterada sem testes muito específicos

--- Worker.py
+++ Worker.py
@@ -3,7 +3,7 @@ import Operations as op
 import subprocess as sp
 from Package import Package
 import NodeManager
-from Except import (ScriptTestErr, InstallErr, TestErr)
+from Except import (ScriptTestErr, InstallErr, TestErr, NoDependencyChange)
 
 class Worker():
 
@@ -22,25 +22,25 @@ class Worker():
 
     # start work
     def start(self):
-        currentDirectory = sp.getstatusoutput('pwd')[1]     # pwd -> /home/user/path/to/BCDetect
+        self.currentDirectory = sp.getstatusoutput('pwd')[1]     # pwd -> /home/user/path/to/BCDetect
 
         self.fullCSV = self.reader.getFull()                # get all versions of csv file
         qtdSucess = 0
         qtdFail = 0
 
-        client_name = self.reader.client_name
-        pathName = 'workspace/' + client_name
+        self.client_name = self.reader.client_name
+        self.pathName = 'workspace/' + self.client_name
 
         sp.getstatusoutput('mkdir workspace/')                          # if this path doesnt exists, create
 
         # if --no-clone was specified
         if self.toClone:
             # clone repository
-            op.clone(self.reader.urlRepo, client_name)
+            op.clone(self.reader.urlRepo, self.client_name)
 
         # file to write
-        writer = open('workspace/' + client_name+'_results.csv', 'w')
-        writer.write("version, version_package, script_test, install, test, node_on_date, node_sucess\n")
+        writer = open('workspace/' + self.client_name+'_results.csv', 'w')
+        writer.write("version, dependency_changed, script_test, install, test, node_on_date, node_sucess\n")
 
         # for each version: ['3.5.0', '3.1.0', '1.0.0', '2.1.0' ...]
         keys = list(self.fullCSV.keys())
@@ -54,126 +54,168 @@ class Worker():
             elif self.oneVersion:
                 continue                        # search to specify version
 
-            release = self.fullCSV[version]     # get the release
+            self.release = self.fullCSV[version]# get the release
 
             # initializing all info variables
-            operation = 'INSTALL'   # current operation
-            script_test = '-'       # has scripts->test
-            codeInstall = '-'       # if get any err in install
-            codeTest = '-'          # if get any err in test
-            version_package = '-'   # the version of package in package.json
-            node_on_date = '-'      # latest node version in date of release
-            node_sucess = '-'       # node version that test had sucess
+            values = {
+                'script_test': '-',       # has scripts->test
+                'codeInstall': '-',       # if get any err in install
+                'codeTest': '-',          # if get any err in test
+                'node_on_date': '-',      # latest node version in date of release
+                'node_sucess': '-',       # node version that test had sucess
+                'dependency_changed': '-'# if none dependency has changed from the latest release
+            }
+
+            # checkout before get package
+            op.commitAll(self.client_name, self.currentDirectory)
+            # change the repository to specify date
+            op.checkout(self.pathName, self.release)
 
             # get the list of node versions and sort decrescent
-            versions = list(NodeManager.nodeVersions.values())
-            versions.sort()
-            versions.reverse()
+            package = Package(self.pathName+'/package.json')
+            versions = self.getListVersions(values, package)
 
-            posCurrentVersion = versions.index()
-            try:
-                op.printTableInfo('   {0}@{1}   {2}--{3}   '.format(client_name, release, release.client_timestamp, release.client_previous_timestamp))
-
-                op.commitAll(client_name, currentDirectory)
-                # change the repository to specify date
-                op.checkout(pathName, release)
-
-                input()
-                # open package.json
-                package = Package(pathName+'/package.json')
-
-                # verify if package.json has test
-                op.verifyTest(package)
-                script_test = 'OK'
-
-                print('    update package.json')
-                op.updatePackage(release, package)
-
-                version_package = package.get('version')
-
-                # get the latest node version of this release
-                versionPackage = NodeManager.getVersion(package, release.client_timestamp)
-
+            # for each version of node before the release date
+            for version_node in versions:
                 try:
-                    print("Test with Node {0}".format(versionPackage))
 
-                    # install all dependencies and test in specify version package
-                    operation = 'INSTALL'
-                    op.npmInstall(pathName, versionPackage)
-                    codeInstall = 'OK'
+                    self.execute(version_node, package, values)
+                    values['node_sucess'] = version_node
 
-                    operation = 'TEST'
-                    op.npmTest(pathName, versionPackage)
-                    codeTest = 'OK'
+                except NoDependencyChange as ex:    # don't wrong
+                    print('EXC: ' + str(ex).upper())
+                    values['dependency_changed'] = 'NO'
+                    break
 
-                except Exception:   # try with latest version of node: 10.9.0
-                    input()
-                    if self.oneTest:    # if dosent want make install and test twice
-                        raise
+                except ScriptTestErr as ex:         # don't wrong
+                    values['script_test'] = 'ERR'
+                    print('EXC: ' + str(ex).upper())
+                    break
 
-                    print('------------\n{0}: ERR\n------------\n'.format(operation))
-                    print("Tentando com Node {0}".format('10.9.0'))
-                    codeInstall = 'ERR'  # if get any err
-                    codeTest = 'ERR'
-                    op.deleteCurrentFolder('{0}/node_modules'.format(client_name))
+                except InstallErr as ex:            # npm install wrong
+                    values['codeInstall'] = 'ERR'
+                    print('ERR: ' + str(ex).upper())
+                    # continue to next node version
 
-                    operation = 'INSTALL'
-                    op.npmInstall(pathName, '10.9.0')
-                    codeInstall = 'OK'
+                except TestErr as ex:               # npm test wrong
+                    values['codeTest'] = 'ERR'
+                    print('ERR: ' + str(ex).upper())
+                    # continue to next node version
 
-                    operation = 'TEST'
-                    op.npmTest(pathName, '10.9.0')
-                    codeTest = 'OK'
+                except Exception as ex:
+                    print("Algum erro inesperado::::::::::::::::::::: " + str(ex))
 
-            except FileNotFoundError as ex: # se não foi possível abrir o package.json
-                qtdFail += 1
-                '''
-                if input().__eq__('OK'):
-                    finalCode = 'OK'
-                    qtdSucess += 1
-                '''
-            except sp.TimeoutExpired as ex: # se a o teste ou o install demorar mais que 10 minutos
-                qtdFail += 1
-                '''
-                if input().__eq__('OK'):
-                    finalCode = 'OK'
-                    qtdSucess += 1
-                '''
-            except ScriptTestErr as ex:     # it's exception, but doesn't err, because package.json doesn't have scripts->test or doesn't be specified
-                print('ERR: ' + str(ex).upper())
-
-            except TestErr as ex:     # it's exception, but doesn't err, because package.json doesn't have scripts->test or doesn't be specified
-                print('ERR: ' + str(ex).upper())
-
-            except InstallErr as ex:     # it's exception, but doesn't err, because package.json doesn't have scripts->test or doesn't be specified
-                print('ERR: ' + str(ex).upper())
-
-            except Exception as ex:
-                qtdFail += 1
-                print('------------\n{0}: ERR: {1}\n------------\n'.format(operation, str(ex)))
-                '''
-                if input().__eq__('OK'):
-                    finalCode = 'OK'
-                    qtdSucess += 1
-                '''
-            else:
-                qtdSucess += 1
+                else:
+                    qtdSucess += 1          # update the count
+                    break                   # quit of for statement
 
             #input()
             # delete folder node_modules
             if self.delete:
-                op.deleteCurrentFolder('{0}/node_modules'.format(client_name))
+                op.deleteCurrentFolder('{0}/node_modules'.format(self.client_name))
 
-            # version, version_package, script_test, install, test, node_on_date, node_sucess
+            # version, dependency_changed, script_test, install, test, node_on_date, node_sucess
             # save result
-            writer.write('{0}, {1}, {2}, {3}, {4}, {5}, {6}\n'.format(release.version, version_package, script_test, codeInstall, codeTest, node_on_date, node_sucess))
+            writer.write('{0}, {1}, {2}, {3}, {4}, {5}, {6}\n'.format(self.release.version, values['dependency_changed'], values['script_test'], values['codeInstall'], values['codeTest'], values['node_on_date'], values['node_sucess']))
 
             if executed:        # if the specify version are executed
                 break
 
         writer.close()
         if self.delete:
-            op.deleteCurrentFolder('{0}'.format(client_name))
+            op.deleteCurrentFolder('{0}'.format(self.client_name))
 
         print("Sucess:", qtdSucess)
         print("Fail:", qtdFail)
+
+
+    # check test, install and test with the specify node version
+    def execute(self, version_node, package, values):
+        try:
+
+            # verify if any dependency has changed
+            operation = 'DEP-CHANGE'
+            self.release.verifyDependencyChange()
+            values['dependency_changed'] = 'YES'
+
+            op.printTableInfo('   {0}@{1}   {2}--{3}   NodeJs-{4}   '.format(self.client_name, self.release, self.release.client_timestamp, self.release.client_previous_timestamp, version_node))
+
+            operation = 'COMMIT'
+            op.commitAll(self.client_name, self.currentDirectory)
+            # change the repository to specify date
+            operation = 'CHECKOUT'
+            op.checkout(self.pathName, self.release)
+
+            #input()
+
+            # verify if package.json has test
+            operation = 'VERIFY-TEST'
+            op.verifyTest(package)
+            values['script_test'] = 'OK'
+
+            print('    update package.json')
+            operation = 'UPDATE-PACKAGE'
+            op.updatePackage(self.release, package)
+
+            # close package.json and save changes
+            package.save()
+
+            # install all dependencies and test in specify version package
+            operation = 'INSTALL'
+            op.npmInstall(self.pathName, version_node)
+            values['codeInstall'] = 'OK'
+
+            operation = 'TEST'
+            op.npmTest(self.pathName, version_node)
+            values['codeTest'] = 'OK'
+
+        except FileNotFoundError as ex: # se não foi possível abrir o package.json
+            raise
+        except sp.TimeoutExpired as ex: # se a o teste ou o install demorar mais que 10 minutos
+            raise
+        except Exception as ex:
+            print('------------\n{0}: ERR: {1}\n------------\n'.format(operation, str(ex)))
+            raise
+
+
+    # return the list of versions node to execute
+    # in future, this will be changed to return the specify version of node
+    def getListVersions(self, values, package):
+
+        # get all node versions
+        versionsNode = list(NodeManager.nodeVersions.values())
+        versionsNode.sort()
+        versionsNode.reverse()
+
+        # get the node version based in date
+        values['node_on_date'] = NodeManager.getVersionOnDate(self.release.client_timestamp)
+
+        # get only versions to execute
+        posCurrentVersion = versionsNode.index(values['node_on_date'])
+        versions = versionsNode[posCurrentVersion:]     # get only versions to execute
+
+        # try get the version in package.json engines->node
+        # if this versions doesn't exists in versions list, insert
+        try:
+            versionEngines = NodeManager.getVersionOnPackage(package)
+
+            if not versions.__contains__(versionEngines):
+                versions.insert(0, versionEngines)
+
+        except KeyError:
+            pass    # do nothing
+
+        print("Versoes que vai rodar " + str(versions))
+        return versions
+
+
+
+from Reader import Reader
+file = 'bubleupventurini.csv'
+reader = Reader(["client_name", "client_version", "client_timestamp", "client_previous_timestamp", "dependency_name","dependency_type", "dependency_resolved_version", "dependency_resolved_version_change"], csvFileName=file)
+fullCSV = reader.getFull()
+keys = list(fullCSV.keys())
+version = keys[0]
+release = fullCSV[version]
+# reader, version, oneTest, clone, delete
+Worker(reader, '-1', False, False, False).start()
