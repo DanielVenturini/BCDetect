9e6befb7f956b87c87df192a47f2f8958a5733af
Daniel Venturini <danielventurini021@gmail.com>
2018-12-16 23:19:33 -0200

    Alterando script de BloxPlot

--- CSV/geraBoxPlot.py
+++ CSV/geraBoxPlot.py
@@ -10,30 +10,47 @@ def getPackage(packageName, current=False):
         return requests.get('https://registry.npmjs.org/'+packageName).json()
 
     try:
-        return json.load(open('./packagejson/npm_packs_2017-06-01/{0}.json'.format(packageName)))
+        package = json.load(open('./packagejson/npm_packs_2017-06-01/{0}.json'.format(packageName)))
+        setLatest(package)
+        return package
     except FileNotFoundError:
-        return {error: "Not found"}
+        return {'error': "Not found"}
+
+def setLatest(package):
+
+    try:
+        versions = list(package['versions'].keys())
+        versions.sort()
+        latest = package['dist-tags']['latest']
+
+        for version in versions:
+            specifyPackage = package['versions'][version]
+            if specifyPackage['version'].__eq__(latest):
+                package['latest'] = specifyPackage
+                return
+
+        package['latest'] = package['versions'][versions[-1]]
+    except:
+        versoes = list(package['versions'].keys())
+        package['latest'] = package['versions'][versoes[-1]]
 
 def getVersoes(package):
     try:
         versoes = list(package['versions'].keys())
         return len(versoes)
-    except KeyError:
+    except:
         return 0
 
 def getScriptTest(package):
     try:
-        versoes = list(package['versions'].keys())
-        return package['versions'][versoes[-1]]['scripts']['test']
-    except KeyError:
+        return package['latest']['scripts']['test']
+    except:
         return 'no test specified'
 
 def getDependencias(package):
     try:
-        versoes = list(package['versions'].keys())
-        dependencies = package['versions'][versoes[-1]]['dependencies']
-        return len(list(dependencies.keys()))
-    except KeyError:
+        return len(list(package['latest']['dependencies'].keys()))
+    except:
         return 0
 
 def getTest(package):
@@ -51,9 +68,9 @@ def resolvUrl(url):
         if url.__contains__('github'):
             return 'https://github.com/' + re.search('[\/|:](\w|-|_|@|:)+\/(\w|-|_|@)+', url).group(0)[1:]
         else:
-            return url
+            return 'https://github.com/danielventurini/false'
     except:
-        return url
+        return 'https://github.com/danielventurini/false'
 
 def getUrl(package):
     try:
@@ -61,11 +78,11 @@ def getUrl(package):
         versoes.sort()
         url = package['versions'][versoes[-1]]['repository']['url']
         return resolvUrl(url)
-    except KeyError:
+    except:
         return 'https://github.com/danielventurini/false'
 
 def verifyExistsRepo(url_repo):
-    return requests.get(url_repo).ok
+    return requests.head(url_repo).ok
 
 def salvaInformacoes(dependencias, releases):
     file = open('dataBoxPlot.js', 'w')
@@ -101,9 +118,12 @@ def geraBoxPlot(arquivo):
 
             qtdDepende = getDependencias(package)
             qtdVersoes = getVersoes(package)
-            test = getTest(package)
-            url_repo = getUrl(package)
-            repo_exists = verifyExistsRepo(url_repo)
+            test = True
+            url_repo = True
+            repo_exists = True
+            #test = getTest(package)
+            #url_repo = getUrl(package)
+            #repo_exists = verifyExistsRepo(url_repo)
 
             print('package: {0}; dependecies: {1}; versions: {2}; test: {3}; url: {4}; exists: {5}'.format(pacote, qtdDepende, qtdVersoes, test, url_repo, repo_exists), end=' - ', flush=True)
             if url_repo and test and repo_exists:
@@ -124,4 +144,4 @@ try:
     else:
         print("USE: python3 sorteador.py todos_pacotes.csv")
 except Exception as ex:
-    print("USE: python3 sorteador.py todos_pacotes.csv: " + str(ex))
+    pass
