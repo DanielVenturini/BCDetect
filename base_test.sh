7b8a930efb3fa71088bea84a911debee66ee2af7
Daniel Venturini <danielventurini021@gmail.com>
2019-05-07 18:16:00 -0300

    Fix npm test with wrong npm i

--- base_test.sh
+++ base_test.sh
@@ -5,5 +5,4 @@ cd $HOME/$BCDETECT/workspace/ && rm -rf *
 cd $HOME/$BCDETECT/
 python3 BCDetect.py $PACKAGE --no-del --j-check-p --only $VERSION
 cd workspace/$PACKAGE/
-npm i
-npm test
+npm i && npm test
