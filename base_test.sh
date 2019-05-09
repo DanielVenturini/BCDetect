6cad0ea7907aafba91e533bc2e281a2b28e24685
Daniel Venturini <danielventurini021@gmail.com>
2019-05-09 00:54:27 -0300

    Add message to separe install from test

--- base_test.sh
+++ base_test.sh
@@ -1,8 +1,9 @@
 export PACKAGE=react-components
 export VERSION=0.0.2
 export BCDETECT=git/bcdetect
+export MESSAGE='\n========================\n= TEST WILL BE STARTED =\n========================\n'
 cd $HOME/$BCDETECT/workspace/ && rm -rf *
 cd $HOME/$BCDETECT/
 python3 BCDetect.py $PACKAGE --no-del --j-check-p --only $VERSION
 cd workspace/$PACKAGE/
-npm i && npm test
+npm i && echo -e $MESSAGE  && npm test
