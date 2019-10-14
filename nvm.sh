e3f801001b601051a155c6f4cf9bf49ca2798f24
venturini <danielventurini021@gmail.com>
2019-10-14 15:07:16 -0300

    Update NVM load

--- nvm.sh
+++ nvm.sh
@@ -15,9 +15,8 @@
 # bash nvm.sh npm test ./path/to/test x.y.z
 
 loads_nvm() {
-	export NVM_DIR="$HOME/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  					# This loads nvm
-	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+	export NVM_DIR="$HOME/.config/nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 }
 
 # get the current version of nvm
